f0eeced47ed7cb2f29c63e8b085327f51a5f0bf3
FeelHippo <mezzasegolas@gmail.com>
2020-05-05 16:31:49 +0200

    little change to index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path')
 module.exports = (directory) => {
     let API_URL = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200504T232731Z.331202c40646801e.6cbac3d503a20f8873ee4072b4bc7045f171f1b9`
     
-    fs.readdir(directory, function(err, filenames) {
+    return fs.readdir(directory, function(err, filenames) {
         if (err) {
             console.log(err);
             return;
